b710678fdf98b2124ec03ada14726ef1e8c63555
Anwesh-Mahapatra <anweshm97@proton.me>
2026-08-17 02:53:19 +0530

    sink ECS docs to k8s-audit-ecs topic

--- pipeline/jobs/k8s_audit_job.py
+++ pipeline/jobs/k8s_audit_job.py
@@ -1,13 +1,21 @@
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors.kafka import KafkaSource, KafkaOffsetsInitializer
+from pyflink.datastream.connectors.kafka import (
+    KafkaSource,
+    KafkaOffsetsInitializer,
+    KafkaSink,
+    KafkaRecordSerializationSchema,
+    DeliveryGuarantee,
+)
 from pyflink.common import SimpleStringSchema, WatermarkStrategy
 import json
 from pyflink.common.typeinfo import Types
 from pipeline.parsers.k8s_audit import parse_audit_event
+from pyflink.datastream.connectors.kafka import KafkaSink, KafkaRecordSerializationSchema
 
 BROKERS = "kafka:9092"
 SOURCE_TOPIC = "k8s-audit-raw"
 GROUP_ID = "flink-k8s-audit"
+SINK_TOPIC = "k8s-audit-ecs"
 
 #Flink and parser wiring (k8s audit events)
 
@@ -55,7 +63,21 @@ def main() -> None:
         .map(to_ecs, output_type=Types.STRING())
         .filter(lambda x: x is not None)
     )
-    ecs.print()
+
+    #Sending it to the sink topic which then connects to elasticsearch SIEM
+    sink = (
+        KafkaSink.builder()
+        .set_bootstrap_servers(BROKERS)
+        .set_record_serializer(
+            KafkaRecordSerializationSchema.builder()
+            .set_topic(SINK_TOPIC)
+            .set_value_serialization_schema(SimpleStringSchema())
+            .build()
+        )
+        .set_delivery_guarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+        .build()
+    )
+    ecs.sink_to(sink)
 
     env.execute("k8s-audit-ecs")
 
